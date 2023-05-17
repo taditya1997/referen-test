8810997f2782dab3eee4910f64dd906bd7f27cc5
Aditya <aditya@Pankajs-MacBook-Air.local>
2023-05-17 14:50:15 +0530

    build fix

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Navbar from "./navbar";
-import "@/styles/globals.css";
-import { Inter } from "next/font/google";
+
 import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export default function Layout({ children }) {
   return (
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href={inter} rel="stylesheet" />
+        <link  rel="stylesheet" />
         <title>Rick and Morty</title>
       </Head>
       <div className="min-h-screen">
